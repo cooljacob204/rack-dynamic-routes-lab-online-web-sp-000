f7ed89450dc7bfb6fabe5ff9e655678d41df763f
Jacob Feldman <jacobgfeld@gmail.com>
2019-02-15 18:15:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,8 +6,8 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if item_name
         item = @@items.find{|i| i.name == item_name}
+      if item_name
         resp.write item.price
       else
         resp.write "Route not found"
