9d519fe439184865b809c22c80a0239a48f0a82f
Jacob Feldman <jacobgfeld@gmail.com>
2019-02-15 18:02:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
       item_name= req.path.split("/items/").last
       item = @@item.find{|i| i.name == item_name}
       
-      resp.write item.price
+      resp.write item
     else
       resp.write "Route not found"
       resp.status = 404
